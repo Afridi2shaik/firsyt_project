08ce1a3ded515abb8037cd490de503df3433d3d3
afridi <afridi2shaik@gmail.com>
2023-10-04 16:32:26 +0530

    first project

---
+++ src/main/java/com/afridi/App.java
@@ -0,0 +1,13 @@
+package com.afridi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
